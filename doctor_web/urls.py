9f5c9f15532ff483b94d30c3e5982842d0194402
Chaithanya R Rao <raochaithanya56@gmail.com>
2025-10-29 21:14:18 +0530

    feat(ui): added delete option in view all patient page

--- doctor_web/urls.py
+++ doctor_web/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.login_view, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('all-patients/', views.view_all_patients, name='view_all_patients'),
+    path('delete-student/<str:usn>/', views.delete_student, name='delete_student'),
+    path('delete-teacher/<str:isn>/', views.delete_teacher, name='delete_teacher'),
 ]
